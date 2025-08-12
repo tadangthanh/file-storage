91411fceda5b66bc7622851e685ff9233c9b1bf6
tathanh <tathanh.mycv@gmail.com>
2025-08-12 21:01:16 +0700

    update

--- metadataservice/src/main/java/vn/thanh/metadataservice/kafkaconfig/KafkaConfig.java
+++ metadataservice/src/main/java/vn/thanh/metadataservice/kafkaconfig/KafkaConfig.java
@@ -1,9 +1,14 @@
 package vn.thanh.metadataservice.kafkaconfig;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 public class KafkaConfig {
@@ -31,4 +36,15 @@ public class KafkaConfig {
         // topicname, partition number,replication number (thuong bang so broker server)
         return new NewTopic(metadataDeleteTopic, 2, (short) 3);
     }
+
+    // cái này sẽ dùng in memory retry, tức là k tạo topic retry
+//    @Bean
+//    DefaultErrorHandler errorHandler(KafkaOperations<String, Object> template) {
+//        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(
+//                template,
+//                (record, ex) -> new TopicPartition(record.topic() + ".DLT", record.partition())
+//        );
+//        return new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 2));
+//    }
+
 }
