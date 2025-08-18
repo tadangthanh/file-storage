df7973d84c6fdce6f5616ceb54512416348cf0dc
tathanh <tathanh.mycv@gmail.com>
2025-08-18 09:19:10 +0700

    udapte

--- metadataservice/src/main/java/vn/thanh/metadataservice/aop/PermissionAspect.java
+++ metadataservice/src/main/java/vn/thanh/metadataservice/aop/PermissionAspect.java
@@ -15,6 +15,9 @@ import vn.thanh.metadataservice.annotation.RequirePermission;
 import vn.thanh.metadataservice.client.PermissionServiceClient;
 import vn.thanh.metadataservice.dto.ResourceType;
 import vn.thanh.metadataservice.exception.AccessDeniedException;
+import vn.thanh.metadataservice.service.ICategoryService;
+import vn.thanh.metadataservice.service.IFileService;
+import vn.thanh.metadataservice.utils.AuthUtils;
 
 import java.lang.reflect.Method;
 import java.util.UUID;
@@ -26,7 +29,8 @@ import java.util.UUID;
 public class PermissionAspect {
 
     private final PermissionServiceClient permissionService; // service bạn đã có sẵn (hasPermission)
-
+    private final IFileService fileService;
+    private final ICategoryService categoryService;
     @Around("@annotation(vn.thanh.metadataservice.annotation.RequirePermission)")
     public Object checkPermission(ProceedingJoinPoint joinPoint) throws Throwable {
         Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
@@ -36,43 +40,46 @@ public class PermissionAspect {
         ResourceType resourceType = annotation.resourceType();
         String resourceParam = annotation.resourceParam();
         int permissionBit = annotation.permissionBit();
-
-        // Lấy request context
-        HttpServletRequest request =
-                ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-        JwtAuthenticationToken auth = (JwtAuthenticationToken) request.getUserPrincipal();
-        String sub = auth.getName();
-        UUID userId;
-        try {
-            userId = UUID.fromString(sub);
-        } catch (IllegalArgumentException e) {
-            log.error("Token sub không phải UUID: {}", sub);
-            throw new AccessDeniedException("Token không hợp lệ");
-        }
-
         // Tìm resourceId từ args
         Object[] args = joinPoint.getArgs();
         String[] paramNames = ((MethodSignature) joinPoint.getSignature()).getParameterNames();
 
         Long resourceId = null;
         for (int i = 0; i < paramNames.length; i++) {
-            if (paramNames[i].equals(resourceParam)) {
-                resourceId = Long.valueOf(args[i].toString());
+            if (resourceParam.equals(paramNames[i])) {
+                if (args[i] instanceof Long id) {
+                    resourceId = id;
+                } else {
+                    throw new IllegalArgumentException("Parameter " + resourceParam + " is not of type Long");
+                }
                 break;
             }
         }
-
         if (resourceId == null) {
             throw new IllegalArgumentException("Missing resourceId in method arguments: " + resourceParam);
         }
+        // Lấy request context
+        UUID userId = AuthUtils.getUserId();
+        // check xem có phải chủ sở hữu tài liệu không, nếu phải thì cho qua, nếu k thì kiểm tra permission
+        boolean isOwner = switch (resourceType) {
+            case DOCUMENT -> fileService.checkUserIsOwner(userId, resourceId);
+            case CATEGORY -> categoryService.checkUserIdOwner(userId, resourceId);
+            default -> false;
+        };
+        if (isOwner) {
+            return joinPoint.proceed();
+        }
 
-        log.info("Check permission for user={} for resourceType={} and resourceId={}", userId, resourceType, resourceId);
-        // Check quyền
+
+        log.info("Checking permission: user={}, type={}, resourceId={}", userId, resourceType, resourceId);
+
         if (!permissionService.hasPermission(userId, resourceId, resourceType, permissionBit).getData()) {
-            log.info("Access denied for user={} for resourceType={} and resourceId={}", userId, resourceType, resourceId);
+            log.warn("Access denied: user={}, type={}, resourceId={}", userId, resourceType, resourceId);
             throw new AccessDeniedException("Bạn không có quyền hạn này");
         }
-        log.info("Permission check passed for user={} for resourceType={} and resourceId={}", userId, resourceType, resourceId);
+
+        log.info("Access granted: user={}, type={}, resourceId={}", userId, resourceType, resourceId);
+
         // Nếu pass
         return joinPoint.proceed();
     }
